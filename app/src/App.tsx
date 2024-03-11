3584c4c726a498242f948082defa7743d7830a72
Teeskid <amaedyteeskid@gmail.com>
2024-03-11 18:29:41 +0100

    app-touched1

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,27 +1,13 @@
 import './App.css'
 
-import reactLogo from './assets/react.svg'
 import Privacy from './pages/Privacy'
-import viteLogo from '/vite.svg'
 
 function App() {
 	return (
-		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Techify NG - Privacy Policy</h1>
-			<div className="card" style={{ textAlign: "left" }}>
-				<p style={{ lineHeight: 1.5 }}>
-					<Privacy />
-				</p>
-			</div>
-		</>
+		<div>
+			<h1>Techify Systems</h1>
+			<Privacy />
+		</div>
 	)
 }
 
--- app/src/pages/Privacy.tsx
+++ app/src/pages/Privacy.tsx
@@ -1,33 +1,36 @@
 export default function Privacy() {
-    return (
-        <ol>
-            <li><strong>Acknowledgment and Acceptance of Terms</strong><br />
-                <strong>Techify NG</strong> is committed to protecting your privacy. This Privacy Statement sets forth our current privacy practices with regard to the information we collect when you or your computer interact with <a href='../'>https://techify.ng</a>. By accessing <a href='../'>https://techify.ng</a>, you acknowledge and fully understand our Privacy Statement and freely consent to the information collection and use practices described in this Website Privacy Statement.
-            </li>
-            <li><strong>Participating Clients, Merchant Policies, and Third Party Websites</strong><br />
-                Related products and offerings with links from this website, including all other websites, have their own privacy statements that can be viewed by clicking on the corresponding links within each respective website. <strong>Techify NG</strong> is not responsible for the privacy practices or contents of third-party or client websites. We recommend and encourage that you always review the privacy policies of merchants and other third parties before you provide any personal information or complete any transaction with such parties.
-            </li>
-            <li><strong>Information We Collect and How We Use It</strong><br />
-                <strong>Techify NG</strong> collects certain information from and about its users three ways: directly from our Web Server logs, the user, and with Cookies. When you visit our Website, we may track information to administer the site and analyze its usage for the purpose of serving our visitors and customers better.
-                This website uses Google Ads’ free conversion tracking features on certain pages. If you contact us online, the destination page will have code on it that will help us understand the path you took to arrive on that page.
+	return (
+	<p style={{ textAlign: "left" }}>
+		<h2>Privacy Policy</h2>
+		<ol>
+			<li><strong>Acknowledgment and Acceptance of Terms</strong><br />
+				<strong>Techify NG</strong> is committed to protecting your privacy. This Privacy Statement sets forth our current privacy practices with regard to the information we collect when you or your computer interact with <a href='../'>https://techify.ng</a>. By accessing <a href='../'>https://techify.ng</a>, you acknowledge and fully understand our Privacy Statement and freely consent to the information collection and use practices described in this Website Privacy Statement.
+			</li>
+			<li><strong>Participating Clients, Merchant Policies, and Third Party Websites</strong><br />
+				Related products and offerings with links from this website, including all other websites, have their own privacy statements that can be viewed by clicking on the corresponding links within each respective website. <strong>Techify NG</strong> is not responsible for the privacy practices or contents of third-party or client websites. We recommend and encourage that you always review the privacy policies of merchants and other third parties before you provide any personal information or complete any transaction with such parties.
+			</li>
+			<li><strong>Information We Collect and How We Use It</strong><br />
+				<strong>Techify NG</strong> collects certain information from and about its users three ways: directly from our Web Server logs, the user, and with Cookies. When you visit our Website, we may track information to administer the site and analyze its usage for the purpose of serving our visitors and customers better.
+				This website uses Google Ads’ free conversion tracking features on certain pages. If you contact us online, the destination page will have code on it that will help us understand the path you took to arrive on that page.
 
-                DoubleClick: We use Google Ads remarketing codes to log when users view specific pages or take specific actions on a website. This allows us to provide targeted advertising in the future. If you do not wish to receive this type of advertising from us in the future you can opt out using the DoubleClick opt-out page or the Telecom Advertising Initiative opt-out page.
+				DoubleClick: We use Google Ads remarketing codes to log when users view specific pages or take specific actions on a website. This allows us to provide targeted advertising in the future. If you do not wish to receive this type of advertising from us in the future you can opt out using the DoubleClick opt-out page or the Telecom Advertising Initiative opt-out page.
 
-                Microsoft Advertising: This website uses Microsoft’s free tracking features on its webpages. If you contact us online, the destination page will have code on it that will help us understand the path you took to arrive on that page.
+				Microsoft Advertising: This website uses Microsoft’s free tracking features on its webpages. If you contact us online, the destination page will have code on it that will help us understand the path you took to arrive on that page.
 
-                We will not disclose personally identifiable information we collect from you to third parties without your permission except to the extent necessary including:
+				We will not disclose personally identifiable information we collect from you to third parties without your permission except to the extent necessary including:
 
-                To fulfill your requests for products.
-                To protect ourselves from liability.
-                To use in marketing and advertising.
-                To respond to legal process or comply with law, or in connection with a merger, acquisition, or liquidation of the company.
-            </li>
-            <li><strong>Changes to This Statement</strong><br />
-                <strong>Techify NG</strong> has the discretion to occasionally update this privacy statement. We encourage you to periodically review this privacy statement to stay informed about how we are helping to protect the personal information we collect.
-            </li>
-            <li><strong>Contacting Us</strong><br />
-                If you have questions regarding our Privacy Statement, its implementation, failure to adhere to this Privacy Statement and/or our general practices, please <a href='../contact-us' className='fw-bold'>contact us</a>.
-            </li>
-        </ol>
-    )
+				To fulfill your requests for products.
+				To protect ourselves from liability.
+				To use in marketing and advertising.
+				To respond to legal process or comply with law, or in connection with a merger, acquisition, or liquidation of the company.
+			</li>
+			<li><strong>Changes to This Statement</strong><br />
+				<strong>Techify NG</strong> has the discretion to occasionally update this privacy statement. We encourage you to periodically review this privacy statement to stay informed about how we are helping to protect the personal information we collect.
+			</li>
+			<li><strong>Contacting Us</strong><br />
+				If you have questions regarding our Privacy Statement, its implementation, failure to adhere to this Privacy Statement and/or our general practices, please <a href='../contact-us' className='fw-bold'>contact us</a>.
+			</li>
+		</ol>
+	</p>
+	)
 }
